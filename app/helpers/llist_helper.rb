c383048fe6c1a2d2ed4e4e1b0f12fa5b9c72472d
Carsten Brodersen <carsten.brodersen@web.de>
2024-02-22 16:48:42 +0100

    watch list draft

---
+++ app/helpers/llist_helper.rb
@@ -0,0 +1,2 @@
+module LlistHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  Rails.application.routes.draw do
+    get "/lists", to: "lists#index"
+    get "/lists/new", to: "lists#new"
+    get "/lists/:id", to: "lists#show"
+    post "/lists/:id", to: "lists#create"
+    # get "/lists", to: "lists#edit"
+    get "/bookmarks", to: "bookmarks#new"
+    post "/bookmarks/:id", to: "bookmarks#create"
+  end
 end
